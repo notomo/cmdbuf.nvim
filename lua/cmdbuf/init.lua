4d6dfe86d8878e08bc53688b75032b4830bfea94
notomo <notomo.motono@gmail.com>
2021-02-01 22:33:26 +0900

    Fix helptags

--- lua/cmdbuf/init.lua
+++ lua/cmdbuf/init.lua
@@ -19,33 +19,33 @@ end
 local M = {}
 
 --- Open a command buffer.
---- @param (optional) |cmdbf.nvim-open-opts|
+--- @param (optional) |cmdbuf.nvim-open-opts|
 function M.open(opts)
   return main("open", {}, opts)
 end
 
 --- Open a command buffer with `split`.
 --- @param (optional) window height
---- @param (optional) |cmdbf.nvim-open-opts|
+--- @param (optional) |cmdbuf.nvim-open-opts|
 function M.split_open(height, opts)
   return main("open", {type = "split", height = height}, opts)
 end
 
 --- Open a command buffer with `vsplit`.
 --- @param (optional) window width
---- @param (optional) |cmdbf.nvim-open-opts|
+--- @param (optional) |cmdbuf.nvim-open-opts|
 function M.vsplit_open(width, opts)
   return main("open", {type = "vsplit", width = width}, opts)
 end
 
 --- Open a command buffer in new tab.
---- @param (optional) |cmdbf.nvim-open-opts|
+--- @param (optional) |cmdbuf.nvim-open-opts|
 function M.tab_open(opts)
   return main("open", {type = "tab"}, opts)
 end
 
 --- Execute the current line command.
---- @param (optional) |cmdbf.nvim-execute-opts|
+--- @param (optional) |cmdbuf.nvim-execute-opts|
 function M.execute(opts)
   return main("execute", opts)
 end
