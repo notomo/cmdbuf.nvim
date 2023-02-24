632928f74b3cbc536af3833badada5ac3564a712
notomo <notomo.motono@gmail.com>
2023-02-24 10:53:11 +0900

    Add type doc

--- lua/cmdbuf/init.lua
+++ lua/cmdbuf/init.lua
@@ -1,15 +1,27 @@
 local M = {}
 
+--- @class CmdbufOpenOption
+--- @field column integer? initial cursor column in the buffer.
+--- @field line integer? set this string to the bottom line in the buffer.
+--- @field open_window fun()? The window after executing this function is used.
+--- @field reusable_window_ids integer[]? force to reuse the window that has the same buffer name. (default: {})
+--- @field type ("vim/cmd"|"vim/sesarch/forward"|"vim/sesarch/backward"|"lua/cmd"|"lua/variable/buffer") handler type (default = "vim/cmd")
+---  - `vim/cmd`: |q:| alternative
+---  - `vim/sesarch/forward`: |q/| alternative
+---  - `vim/sesarch/backward`: |q?| alternative
+---  - `lua/cmd`: |q:| alternative for lua command
+---  - `lua/variable/buffer`: buffer variable and command
+
 --- Open a command buffer.
---- @param opts table|nil: |cmdbuf.nvim-open-opts|
+--- @param opts CmdbufOpenOption?: |CmdbufOpenOption|
 function M.open(opts)
   opts = opts or {}
   require("cmdbuf.command").open(opts)
 end
 
 --- Open a command buffer with `split`.
---- @param height number|nil: window height
---- @param opts table|nil: |cmdbuf.nvim-open-opts|
+--- @param height integer|nil: window height
+--- @param opts CmdbufOpenOption?: |CmdbufOpenOption|
 function M.split_open(height, opts)
   opts = opts or {}
   opts.open_window = opts.open_window or require("cmdbuf.layout").split_layout(height)
@@ -17,8 +29,8 @@ function M.split_open(height, opts)
 end
 
 --- Open a command buffer with `vsplit`.
---- @param width number|nil: window width
---- @param opts table|nil: |cmdbuf.nvim-open-opts|
+--- @param width integer|nil: window width
+--- @param opts CmdbufOpenOption?: |CmdbufOpenOption|
 function M.vsplit_open(width, opts)
   opts = opts or {}
   opts.open_window = opts.open_window or require("cmdbuf.layout").vsplit_layout(width)
@@ -26,21 +38,24 @@ function M.vsplit_open(width, opts)
 end
 
 --- Open a command buffer in new tab.
---- @param opts table|nil: |cmdbuf.nvim-open-opts|
+--- @param opts CmdbufOpenOption?: |CmdbufOpenOption|
 function M.tab_open(opts)
   opts = opts or {}
   opts.open_window = opts.open_window or require("cmdbuf.layout").tab_layout()
   require("cmdbuf.command").open(opts)
 end
 
+--- @class CmdbufExecuteOption
+--- @field quit boolean? whether quit the window after execution.
+
 --- Execute the current line command.
---- @param opts table|nil: |cmdbuf.nvim-open-opts|
+--- @param opts CmdbufExecuteOption?: |CmdbufExecuteOption|
 function M.execute(opts)
   require("cmdbuf.command").execute(opts)
 end
 
 --- Delete current line (or given range) from command history
---- @param range table|nil: 1-based range {start number, end number}
+--- @param range integer[]?: 1-based range {start number, end number}
 function M.delete(range)
   require("cmdbuf.command").delete(range)
 end
--- spec/lua/cmdbuf/doc.lua
+++ spec/lua/cmdbuf/doc.lua
@@ -12,48 +12,19 @@ require("genvdoc").generate(plugin_name .. ".nvim", {
         return "Lua module: " .. group
       end,
       group = function(node)
-        if node.declaration == nil then
+        if node.declaration == nil or node.declaration.type ~= "function" then
           return nil
         end
         return node.declaration.module
       end,
     },
     {
-      name = "OPTIONS",
-      body = function(ctx)
-        local open_opts_text
-        do
-          local descriptions = {
-            line = [[(number | nil): set this string to the bottom line in the buffer.]],
-            column = [[(number | nil): initial cursor column in the buffer.]],
-            reusable_window_ids = [[(list | nil): force to reuse the window that has the same buffer name. (default: {})]],
-            open_window = [[(function | nil): The window after executing this function is used.]],
-            type = [[(string | nil): handler type (default = "vim/cmd")
-  - `vim/cmd`: |q:| alternative
-  - `vim/sesarch/forward`: |q/| alternative
-  - `vim/sesarch/backward`: |q?| alternative
-  - `lua/cmd`: |q:| alternative for lua command
-  - `lua/variable/buffer`: buffer variable and command]],
-          }
-          local keys = vim.tbl_keys(descriptions)
-          local lines = util.each_keys_description(keys, descriptions)
-          open_opts_text = table.concat(lines, "\n")
+      name = "STRUCTURE",
+      group = function(node)
+        if node.declaration == nil or node.declaration.type ~= "class" then
+          return nil
         end
-
-        local execute_opts_text
-        do
-          local descriptions = {
-            quit = [[(boolean | nil): whether quit the window after execution.]],
-          }
-          local keys = vim.tbl_keys(descriptions)
-          local lines = util.each_keys_description(keys, descriptions)
-          execute_opts_text = table.concat(lines, "\n")
-        end
-
-        return util.sections(ctx, {
-          { name = "|cmdbuf.open()| options", tag_name = "open-opts", text = open_opts_text },
-          { name = "|cmdbuf.execute()| options", tag_name = "execute-opts", text = execute_opts_text },
-        })
+        return "STRUCTURE"
       end,
     },
     {
@@ -67,7 +38,7 @@ CmdbufNew                                              *cmdbuf.nvim-CmdbufNew*
     {
       name = "EXAMPLES",
       body = function()
-        return require("genvdoc.util").help_code_block_from_file(example_path, { language = "lua" })
+        return util.help_code_block_from_file(example_path, { language = "lua" })
       end,
     },
   },
