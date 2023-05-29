ce0d6b0a5a3d289a1ba5fad3aaa29f5d909cd08f
atusy <30277794+atusy@users.noreply.github.com>
2023-05-29 17:19:39 +0900

    feat: support limiting the number of histories

--- lua/cmdbuf/lib/history.lua
+++ lua/cmdbuf/lib/history.lua
@@ -5,15 +5,23 @@ function M.delete(name, str)
   vim.fn.histdel(name, pattern)
 end
 
-function M.filter_map(name, f)
-  vim.validate({ name = { name, "string" }, f = { f, "function" } })
+function M.filter_map(name, f, n)
+  vim.validate({
+    name = { name, "string" },
+    f = { f, "function" },
+    n = { n, function(x) return x == nil or type(x) == "number" end, "number or nil" },
+  })
   local count = vim.fn.histnr(name)
+  n = n or count
   local cmds = {}
-  for i = 1, count, 1 do
+  for i = count, 1, -1 do
     local history = vim.fn.histget(name, i)
     local cmd = f(history)
     if cmd then
-      table.insert(cmds, cmd)
+      table.insert(cmds, 1, cmd)
+    end
+    if #cmds >= n then
+      return cmds
     end
   end
   return cmds
