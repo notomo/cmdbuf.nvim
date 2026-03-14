9de81b38786b3039063998fc67351dc38a7f66f1
notomo <notomo.motono@gmail.com>
2026-03-14 09:06:26 +0900

    Update vendor

--- lua/cmdbuf/vendor/misclib/cursor.lua
+++ lua/cmdbuf/vendor/misclib/cursor.lua
@@ -28,8 +28,19 @@ function M.set(position, window_id)
   window_id = window_id or 0
   local bufnr = vim.api.nvim_win_get_buf(window_id)
   local count = vim.api.nvim_buf_line_count(bufnr)
-  local row = count >= position[1] and position[1] or count
-  vim.api.nvim_win_set_cursor(window_id, { row, position[2] })
+  local row = math.max(1, position[1])
+  row = math.min(row, count)
+  local column = position[2]
+
+  local ok = pcall(function()
+    vim.api.nvim_win_set_cursor(window_id, { row, column })
+  end)
+  if ok then
+    return
+  end
+
+  vim.api.nvim_win_set_cursor(window_id, { row, 0 })
+  M.set_column(column, window_id)
 end
 
 return M
